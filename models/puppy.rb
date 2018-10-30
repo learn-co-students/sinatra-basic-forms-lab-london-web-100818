196ad71c31e94bf1b02ecb4b3b742d7f00fca24c
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-10-30 07:40:14 +0000

    Done.

---
+++ models/puppy.rb
@@ -0,0 +1,16 @@
+class Puppy
+    attr_accessor :name, :breed, :age
+
+    @@all = []
+
+    def initialize(params)
+        @name = params[:name]
+        @breed = params[:breed]
+        @age = params[:age]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
